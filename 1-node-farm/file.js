0d47c1f95ffc424f6e66ae6a5c31cc3b098b645c
Ryan Su <ryansu.1218@qq.com>
2023-05-23 20:28:21 +0800

    Finish node-farm.

---
+++ 1-node-farm/file.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+// Synchronous -> Blocking 👎🏻
+const textIn = fs.readFileSync(`${__dirname}/txt/input.txt`, 'utf-8');
+console.log(textIn);
+
+// Asynchronous -> Non-Blocking 👍🏻
+// But Multi-layer Callback Function -> Callback Hell 🤯
+fs.readFile(`${__dirname}/txt/start.txt`, 'utf-8', (err, data1) => {
+	if (err) throw err;
+	fs.readFile(`${__dirname}/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+		if (err) throw err;
+		fs.readFile(`${__dirname}/txt/append.txt`, 'utf-8', (err, data3) => {
+			if (err) throw err;
+			fs.writeFile(`${__dirname}/txt/final.txt`, `${data2}\n${data3}`, 'utf-8', err => {
+				if (err) throw err;
+				console.log('Done! 👌🏻');
+			});
+		});
+	});
+});
+console.log('Reading and writing file...');
+
+// Wringting text to a file
+const textOut = `This is what we know about avocado: ${textIn}.\n\nCreated on ${Date.now()}.`;
+
+fs.writeFileSync(`${__dirname}/txt/output.txt`, textOut, 'utf-8');
+console.log('File written!');
---
+++ 1-node-farm/index.js
@@ -0,0 +1,61 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const replaceTemplate = require(`${__dirname}/modules/replaceTemplate`);
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+	const { query, pathname } = url.parse(req.url, true);
+
+	// Overview page
+	if (pathname === '/' || pathname === '/overview') {
+		res.writeHead(200, {
+			'Content-Type': 'text/html',
+		});
+
+		const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+
+		const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+		res.end(output);
+
+		// Product page
+	} else if (pathname === '/product') {
+		res.writeHead(200, {
+			'Content-Type': 'text/html',
+		});
+
+		const product = dataObj[query.id];
+		const output = replaceTemplate(tempProduct, product);
+
+		res.end(output);
+
+		// API
+	} else if (pathname === '/api') {
+		res.writeHead(200, {
+			'Content-Type': 'application/json',
+		});
+		res.end(data);
+
+		// 404
+	} else {
+		fs.readFile(`${__dirname}/404/404.html`, function (err, data) {
+			if (err) throw err;
+			res.writeHead(404, { 'Content-Type': 'text/html', 'my-own-header': 'none' });
+			res.write(data);
+			res.end();
+		});
+	}
+
+	// res.end('Hello from the server!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+	console.log('Listing to requests on port 8000');
+});
