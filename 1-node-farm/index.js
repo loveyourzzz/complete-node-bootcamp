7373e0214b78e5adf1c6a3a6d26c2f37a452dc5d
Ryan Su <ryansu.1218@qq.com>
2023-05-24 10:54:15 +0800

    Add Slugify.

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const slugify = require('slugify');
 const replaceTemplate = require(`${__dirname}/modules/replaceTemplate`);
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
@@ -9,6 +10,8 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
 
 const server = http.createServer((req, res) => {
 	const { query, pathname } = url.parse(req.url, true);
